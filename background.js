18d3d79c9be28104c8cb7a300e255dec8d85a70d
Scott Perlitch <scottperlitch@Scotts-MacBook-Pro.local>
2015-09-20 16:17:00 -0400

    Clean up options

--- background.js
+++ background.js
@@ -1,115 +1,85 @@
-var checkInterval = 1500,
-		notified = false,
-		highUsage = 70,
-		prevCPUinfo = false,
-		percentageStats = [];
+(function() {
+	var delay = 0.1,
+			duration = 1,
+			intervals = 4,
+			checkInterval = duration / intervals,
+			highUsage = 70,
+			prevCPUinfo = false,
+			percentageStats = [];
 
-function options() { 
-	chrome.storage.sync.get({
-		browserinterval: 1,
-		highUsage: 70,
-		checkInterval: 3300,
-		duration: 120000,
-	}, function(items) {
-		duration = items.duration;
-		checkInterval = items.checkInterval;
-		highUsage = items.highUsage;
+	function options() { 
+		chrome.storage.sync.get(
+				{
+					highUsage: highUsage,
+					duration: duration,
+					checkInterval: checkInterval,
+				},
+				function(items) {
+					duration = items.duration;
+					checkInterval = items.checkInterval;
+					highUsage = items.highUsage;
+				});
+	};
 
-		console.log(items);
-	});
-};
-
-function setIcon(color) {
-	chrome.browserAction.setIcon({ 
-		path: {
-			'19': 'img/icon19-'+color+'.png',
-			'38': 'img/icon38-'+color+'.png'
-		}
-	});
-}
-
-window.addEventListener("load", options);
-
-chrome.storage.onChanged.addListener(options);
-
-//chrome.notifications.onClicked.addListener(function() {
-//	chrome.tabs.create({
-//		url: "options.html"
-//	});
-//});
-//
-//chrome.notifications.onClosed.addListener(function() { 
-//	notified = false
-//});
-
-window.addEventListener("load", function updateStats() {
-	chrome.system.cpu.getInfo(function(info) {
-		var totalUsage = 0,
-		i = 0,
-		percent = 0;
-
-		if (prevCPUinfo) {
-			for (; i < info.numOfProcessors; i++) {
-				var usage = info.processors[i].usage;
-				var prevUsage = prevCPUinfo.processors[i].usage;
-				var user = (prevUsage.user - usage.user) / (prevUsage.total - usage.total) * 100;
-				var kernel = (prevUsage.kernel - usage.kernel) / (prevUsage.total - usage.total) * 100;
-				totalUsage = totalUsage + user + kernel;
-			} 
-		}
-
-		currentPercent = Math.round(totalUsage / info.numOfProcessors);
-		percentageStats.push(currentPercent);
-		prevCPUinfo = info;
-		console.log(percentageStats);
-
-		if (percentageStats.length > 4) {
-			percentageStats.shift();
-			console.log(percentageStats);
-			var arePercentagesHigh = percentageStats.every(function(num) {
-				return num > 10;
-			});
-			console.log(arePercentagesHigh);
-			if (arePercentagesHigh) {
-				setIcon('red');
-				//if ( ! notified ) {
-				//	chrome.notifications.create("highcpu", {
-				//		type: "progress", 
-				//		title: "Simple CPU",
-				//		message: "Warning! Your CPU usage is high!",
-				//		iconUrl: "img/icon128.png",
-				//		progress: percent
-				//	}, function() { 
-				//		notified = true
-				//	});
-				//}
-				//else if (notified === true) { chrome.notifications.clear("highcpu",
-				//		function() { notified = false }
-				//		) }
-			} else {
-				setIcon('gray');
-				//notified = false;
+	function setIcon(color) {
+		chrome.browserAction.setIcon({ 
+			path: {
+				'19': 'img/icon19-'+color+'.png',
+				'38': 'img/icon38-'+color+'.png'
 			}
-		}
-		//if (percent > highUsage && notified === false) {
-		//	chrome.notifications.create("highcpu", {
-		//		type: "progress", 
-		//		title: "Simple CPU",
-		//		message: "Warning! Your CPU usage is high!",
-		//		iconUrl: "img/icon128.png",
-		//		progress: percent
-		//	}, function() { 
-		//		notified = true
-		//	})
-		//}
-		//else if (percent > highUsage && notified === true) {
-		//	chrome.notifications.update("highcpu", {
-		//		progress: percent
-		//	}, function() { } );
-		//}
-		//else if (notified === true) { chrome.notifications.clear("highcpu",
-		//		function() { notified = false }
-		//		) }
+		});
+	}
+
+	function updateStats() {
+		chrome.system.cpu.getInfo(function(info) {
+			var totalUsage = 0,
+			i = 0,
+			percent = 0;
+
+			if (prevCPUinfo) {
+				for (; i < info.numOfProcessors; i++) {
+					var usage = info.processors[i].usage;
+					var prevUsage = prevCPUinfo.processors[i].usage;
+					var user = (prevUsage.user - usage.user) / (prevUsage.total - usage.total) * 100;
+					var kernel = (prevUsage.kernel - usage.kernel) / (prevUsage.total - usage.total) * 100;
+					totalUsage = totalUsage + user + kernel;
+				} 
+			}
+
+			currentPercent = Math.round(totalUsage / info.numOfProcessors);
+			percentageStats.push(currentPercent);
+			prevCPUinfo = info;
+			console.log(percentageStats);
+
+			chrome.browserAction.setTitle({ 
+				title: "Using " + currentPercent + "%"
+			});
+
+			if (percentageStats.length > intervals) {
+				percentageStats.shift();
+				console.log(percentageStats);
+				var arePercentagesHigh = percentageStats.every(function(num) {
+					return num > highUsage;
+				});
+				console.log(arePercentagesHigh);
+				if (arePercentagesHigh) {
+					setIcon('red');
+				} else {
+					setIcon('gray');
+				}
+			}
+		});
+	}
+
+	window.addEventListener("load", options);
+	chrome.storage.onChanged.addListener(options);
+
+	chrome.alarms.create('updateStats', {
+		delayInMinutes: delay,
+		periodInMinutes: checkInterval
 	});
-	setTimeout(updateStats, checkInterval);
-});
+
+	chrome.alarms.onAlarm.addListener(function( alarmInfo ) {
+		updateStats();
+	});
+})();
--- options.js
+++ options.js
@@ -1,43 +1,46 @@
-var app = true;
+(function() {
+	var duration = 1,
+			intervals = 4,
+			checkInterval = duration / intervals,
+			highUsage = 70,
+			$ = function(el) { return document.querySelector(el); };
 
-chrome.storage.sync.get({
-	// me
-	duration: 120000,
-	checkInterval: 1000,
-	highUsage: 70
-}, function(items) {
-	// me
-	document.querySelector("#checkInterval").value = items.checkInterval / 1000;
-	document.querySelector("#highUsage").value = items.highUsage;
-	document.querySelector("#duration").value = items.duration / 60000;
-	console.log('options.js');
-	console.log(items);
-});
+	chrome.storage.sync.get(
+			{
+				highUsage: highUsage,
+				duration: duration,
+				checkInterval: checkInterval,
+			},
+			function(items) {
+				$("#highUsage").value = items.highUsage;
+				$("#duration").value = items.duration;
+				console.log('options.js', items);
+			}
+	);
 
-chrome.system.cpu.getInfo(function(info) {
-	document.querySelector("#processors").innerText = info.numOfProcessors;
-});
+	$("form").addEventListener("submit", function(event) {
+		event.preventDefault();
+		if ( $('#duration').value < 1 ) {
+			alert('Duration must be greater than 1');
+			return;
+		}
+		chrome.storage.sync.set({
+			checkInterval: $("#duration").value / 4,
+			highUsage: $('#highUsage').value,
+			duration: $('#duration').value
+		}, function() {
+			$("input[type='submit']").value = "Saved sucessfully";
+			setTimeout(function() { 
+				$("input[type='submit']").value = "Save"
+			}, 2000);
+		} );
+	});
 
-document.querySelector("form").addEventListener("submit", function(event) {
-	event.preventDefault();
+	$("form").addEventListener("reset", function(event) {
+		event.preventDefault();
+		var reset = confirm("Are you sure you want to reset all options?");
+		if (reset == true) {
+			chrome.storage.sync.clear(function() { chrome.tabs.reload()});
+		} });
 
-	// SET
-	chrome.storage.sync.set({
-		checkInterval: document.querySelector("#checkInterval").value * 1000,
-		highUsage: document.querySelector('#highUsage').value,
-		duration: document.querySelector('#duration').value * 60000
-	}, function() {
-		document.querySelector("input[type='submit']").value = "Saved sucessfully";
-		setTimeout(function() { 
-			document.querySelector("input[type='submit']").value = "Save"
-		}, 2000);
-	} );
-});
-
-
-document.querySelector("form").addEventListener("reset", function(event) {
-  event.preventDefault();
-  var reset = confirm("Are you sure you want to reset all options?");
-  if (reset == true) {
-    chrome.storage.sync.clear(function() { chrome.tabs.reload()});
-} });
+})();
