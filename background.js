d6ad92d054cdeb7619e66fdef75dc461664008ab
Scott Perlitch <scottperlitch@Scotts-MacBook-Pro.local>
2015-09-21 11:05:03 -0400

    Reorganize background.js pattern

--- background.js
+++ background.js
@@ -1,85 +1,101 @@
 (function() {
-	var delay = 0.1,
-			duration = 1,
-			intervals = 4,
-			checkInterval = duration / intervals,
-			highUsage = 70,
-			prevCPUinfo = false,
-			percentageStats = [];
+	var s,
+	CPU = {
 
-	function options() { 
-		chrome.storage.sync.get(
-				{
-					highUsage: highUsage,
-					duration: duration,
-					checkInterval: checkInterval,
-				},
-				function(items) {
-					duration = items.duration;
-					checkInterval = items.checkInterval;
-					highUsage = items.highUsage;
-				});
-	};
+		settings: {
+			delay: 0.1,
+			duration: 1,
+			intervals: 4,
+			checkInterval: 0.25,
+			highUsage: 5,
+		},
 
-	function setIcon(color) {
-		chrome.browserAction.setIcon({ 
-			path: {
-				'19': 'img/icon19-'+color+'.png',
-				'38': 'img/icon38-'+color+'.png'
-			}
-		});
-	}
+		init: function init() {
+			s = this.settings;
+			this.percentageStats = [];
+			this.prevCPUinfo = null,
+			this.setOptions();
+			this.setAlarm();
+			chrome.storage.onChanged.addListener(this.setOptions());
+		},
 
-	function updateStats() {
-		chrome.system.cpu.getInfo(function(info) {
-			var totalUsage = 0,
-			i = 0,
-			percent = 0;
+		setOptions: function setOptions() {
+			chrome.storage.sync.get(
+					{
+						highUsage:			s.highUsage,
+						duration:				s.duration,
+						checkInterval:	s.checkInterval,
+					},
+					function(syncedItems) {
+						s.duration =			syncedItems.duration;
+						s.checkInterval = syncedItems.checkInterval;
+						s.highUsage =			syncedItems.highUsage;
+					});
+		},
 
-			if (prevCPUinfo) {
-				for (; i < info.numOfProcessors; i++) {
-					var usage = info.processors[i].usage;
-					var prevUsage = prevCPUinfo.processors[i].usage;
-					var user = (prevUsage.user - usage.user) / (prevUsage.total - usage.total) * 100;
-					var kernel = (prevUsage.kernel - usage.kernel) / (prevUsage.total - usage.total) * 100;
-					totalUsage = totalUsage + user + kernel;
-				} 
-			}
-
-			currentPercent = Math.round(totalUsage / info.numOfProcessors);
-			percentageStats.push(currentPercent);
-			prevCPUinfo = info;
-			console.log(percentageStats);
-
-			chrome.browserAction.setTitle({ 
-				title: "Using " + currentPercent + "%"
+		setAlarm: function setAlarm() {
+			var self = this;
+			chrome.alarms.create('updateStats', {
+				delayInMinutes:  s.delay,
+				periodInMinutes: s.checkInterval
 			});
+			chrome.alarms.onAlarm.addListener(function( alarmInfo ) {
+				self.updateStats();
+			});
+		},
 
-			if (percentageStats.length > intervals) {
-				percentageStats.shift();
-				console.log(percentageStats);
-				var arePercentagesHigh = percentageStats.every(function(num) {
-					return num > highUsage;
-				});
-				console.log(arePercentagesHigh);
-				if (arePercentagesHigh) {
-					setIcon('red');
-				} else {
-					setIcon('gray');
+		updateStats: function updateStats() {
+			var self = this,
+					totalUsage = 0,
+					i = 0,
+					percent = 0,
+					arePercentagesHigh = false;
+
+			chrome.system.cpu.getInfo(function(info) {
+				if (self.prevCPUinfo) {
+					for (; i < info.numOfProcessors; i++) {
+						var usage = info.processors[i].usage;
+						var prevUsage = self.prevCPUinfo.processors[i].usage;
+						var user = (prevUsage.user - usage.user) / (prevUsage.total - usage.total) * 100;
+						var kernel = (prevUsage.kernel - usage.kernel) / (prevUsage.total - usage.total) * 100;
+						totalUsage += user + kernel;
+					} 
 				}
-			}
-		});
+
+				currentPercent = Math.round(totalUsage / info.numOfProcessors);
+				self.percentageStats.push(currentPercent);
+				self.prevCPUinfo = info;
+				console.log(self.percentageStats);
+				chrome.browserAction.setTitle({ 
+					title: "using " + currentPercent + "% cpu"
+				});
+
+				if (self.percentageStats.length > s.intervals) {
+					self.percentageStats.shift();
+					console.log(self.percentageStats);
+					arePercentagesHigh = self.percentageStats.every(function(num) {
+						return num > s.highUsage;
+					});
+					console.log(arePercentagesHigh);
+					if (arePercentagesHigh) {
+						self.setIcon('red');
+					} else {
+						self.setIcon('gray');
+					}
+				}
+			});
+		},
+
+		setIcon: function setIcon(color) {
+			chrome.browserAction.setIcon({ 
+				path: {
+					'19': 'img/icon19-'+color+'.png',
+					'38': 'img/icon38-'+color+'.png'
+				}
+			});
+		}
 	}
 
-	window.addEventListener("load", options);
-	chrome.storage.onChanged.addListener(options);
+	CPU.init();
 
-	chrome.alarms.create('updateStats', {
-		delayInMinutes: delay,
-		periodInMinutes: checkInterval
-	});
-
-	chrome.alarms.onAlarm.addListener(function( alarmInfo ) {
-		updateStats();
-	});
-})();
+}());
